c2acd243a5de7b7934177c769dd28e54dcc9c230
Marinella Moral <moral1820393@mkt.ceu.edu.ph>
2022-03-26 14:15:12 +0800

    Additional Hardware Info

--- app/src/main/java/com/example/focuslyplanb/InfoActivity.java
+++ app/src/main/java/com/example/focuslyplanb/InfoActivity.java
@@ -6,13 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class InfoActivity extends AppCompatActivity {
+
+    TextView about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        about = findViewById(R.id.about_text);
+
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.about_focusly);
+            }
+        });
+    }
+
+    public void goBackInfo(View info){
+        Intent i = new Intent(this, InfoActivity.class);
+        startActivity(i);
     }
 
     public void goBack(View back){
